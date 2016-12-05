ab935032281edaf3173de8438fe1a49107bdcea6
Fingo <Fingo@DESKTOP-6R1NG8F>
2016-12-05 15:34:49 +0100

    Add project files.

---
+++ src/JwtAuthServer/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace JwtAuthServer.Controllers
+{
+    [Route("Version/[controller]")]
+    public class VersionController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string version = Assembly.GetEntryAssembly().GetName().Version.ToString();
+            Debug.WriteLine(version);
+            return Json(version);
+        }
+    }
+}
