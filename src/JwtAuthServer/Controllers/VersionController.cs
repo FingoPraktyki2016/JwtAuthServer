d3044d106abe6153d01bb6b30c5581688725143b
SilentGreg <chybzinski.grzegorz@gmail.com>
2016-12-05 16:20:26 +0100

    AS-1 Fixed routing

--- src/JwtAuthServer/Controllers/VersionController.cs
+++ src/JwtAuthServer/Controllers/VersionController.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Reflection;
-using System.Diagnostics;
 
 namespace JwtAuthServer.Controllers
 {
-    [Route("Version/[controller]")]
+    [Route("api/[controller]")]
     public class VersionController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public JsonResult Get()
+        public JsonResult Version()
         {
-            string version = Assembly.GetEntryAssembly().GetName().Version.ToString();          
+            string version = Assembly.GetEntryAssembly().GetName().Version.ToString();
             return Json(version);
         }
     }
