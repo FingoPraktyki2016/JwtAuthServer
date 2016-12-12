db7ec56d31b39133215239bcc3dbec44488fb371
Fingo <Fingo@DESKTOP-JVFKU46>
2016-12-12 16:04:03 +0100

    AS-8:  Added new BusinessLogic skeleton

---
+++ src/BusinessLogic/JwtParser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+
+namespace LegnicaIT.BusinessLogic
+{
+    public class JwtParser
+    {
+        private static string SecretKey = "LegnicaIT-Fingo-JWT-KEY";
+
+        public JwtParser()
+        {
+            
+        }
+
+
+        public Boolean Verify(String token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            SecurityToken validatedToken;
+            //handler.ValidateToken(token, TokenValidationParameters.DefaultAuthenticationType, out validatedToken);
+
+
+            return true;
+        }
+
+
+    }
+}
---
+++ src/JwtAuthServer/Models/VerifyTokenModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JwtAuthServer.Models
+{
+    public class VerifyTokenModel
+    {
+        public string Token { get; set; }
+
+    }
+}
