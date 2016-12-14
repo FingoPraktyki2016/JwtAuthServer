f335e1202c055683e47aed5e9dd2af7574ce34bc
Radziejwskig <radziejewski.g@gmail.com>
2016-12-14 08:46:31 +0100

    AS-8: Updated verify skeleton

--- src/BusinessLogic/JwtParser.cs
+++ src/BusinessLogic/JwtParser.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
 
@@ -12,19 +9,38 @@ namespace LegnicaIT.BusinessLogic
         private static string SecretKey = "LegnicaIT-Fingo-JWT-KEY";
 
         public JwtParser()
-        {
-            
+        {   
         }
 
-
-        public Boolean Verify(String token)
+        public bool Verify(string token)
         {
             var handler = new JwtSecurityTokenHandler();
-            SecurityToken validatedToken;
-            //handler.ValidateToken(token, TokenValidationParameters.DefaultAuthenticationType, out validatedToken);
+            SecurityToken validatedToken = null;
+            TokenValidationParameters parameters = new TokenValidationParameters()
+            { 
+                ValidateLifetime = true,
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateIssuerSigningKey = true
+            };
+            bool result = false;
 
+            try
+            {
+                handler.ValidateToken(token, parameters, out validatedToken);
+                result = true;
+            }
+            catch (ArgumentNullException e)
+            {
+            }
+            catch (ArgumentException e)
+            {
+            }
+            catch (Exception e)
+            {
+            }
 
-            return true;
+            return result;
         }
 
 
