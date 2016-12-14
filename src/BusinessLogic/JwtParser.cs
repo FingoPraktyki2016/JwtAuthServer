7c4c26a365e2ddb648acc14096efc1dfddacdfc1
Saibamen <saibamenppl@gmail.com>
2016-12-14 15:03:56 +0100

    Code cleanup and small fixes by #ReSharper

--- src/BusinessLogic/JwtParser.cs
+++ src/BusinessLogic/JwtParser.cs
@@ -9,18 +9,14 @@ namespace LegnicaIT.BusinessLogic
     {
         // Move away from GIT repo!!!!
         private static string IssuerName = "LegnicaIT";
+
         private static string SecretKey = "LegnicaIT-Fingo-JWT-KEY";
 
         private static readonly SymmetricSecurityKey encodedSecretKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(SecretKey));
 
-        public JwtParser()
-        {
-        }
-
         public bool Verify(string token)
         {
             var handler = new JwtSecurityTokenHandler();
-            SecurityToken validatedToken = null;
             TokenValidationParameters parameters = new TokenValidationParameters()
             {
                 ValidateLifetime = true,
@@ -35,6 +31,7 @@ namespace LegnicaIT.BusinessLogic
 
             try
             {
+                SecurityToken validatedToken;
                 handler.ValidateToken(token, parameters, out validatedToken);
                 result = true;
             }
@@ -56,10 +53,10 @@ namespace LegnicaIT.BusinessLogic
             var handler = new JwtSecurityTokenHandler();
             var credentials = new SigningCredentials(encodedSecretKey, SecurityAlgorithms.HmacSha256Signature);
 
-            var identity = new ClaimsIdentity(new Claim[]
+            var identity = new ClaimsIdentity(new[]
             {
                 new Claim(ClaimTypes.Email, formEmail),
-                new Claim("iss", IssuerName), 
+                new Claim("iss", IssuerName),
                 new Claim("AppId", formAppId.ToString())
             });
 
