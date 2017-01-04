40de99a09e5062a124cf3902b020a4777760e45a
Szymon Matwijów <szymon.matwijow@gmail.com>
2017-01-04 11:41:34 +0100

    repair migrations

--- src/DataAccess/Migrations/20170104103904_init.Designer.cs
+++ src/DataAccess/Migrations/20170104103904_init.Designer.cs
@@ -8,7 +8,7 @@ using LegnicaIT.DataAccess.Context;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(JwtDbContext))]
-    [Migration("20170104084658_init")]
+    [Migration("20170104103904_init")]
     partial class init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -37,6 +37,26 @@ namespace DataAccess.Migrations
                     b.ToTable("Apps");
                 });
 
+            modelBuilder.Entity("LegnicaIT.DataAccess.Models.Role", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreatedOn");
+
+                    b.Property<DateTime>("DeletedOn");
+
+                    b.Property<DateTime>("ModifiedOn");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("NVARCHAR(150)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Roles");
+                });
+
             modelBuilder.Entity("LegnicaIT.DataAccess.Models.User", b =>
                 {
                     b.Property<int>("Id")
@@ -63,12 +83,41 @@ namespace DataAccess.Migrations
                         .IsRequired()
                         .HasColumnType("NVARCHAR(256)");
 
-                    b.Property<byte[]>("PasswordSalt")
-                        .IsRequired();
+                    b.Property<string>("PasswordSalt")
+                        .IsRequired()
+                        .HasColumnType("NVARCHAR(128)");
 
                     b.HasKey("Id");
 
                     b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("LegnicaIT.DataAccess.Models.UserAppRole", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("AppId");
+
+                    b.Property<DateTime>("CreatedOn");
+
+                    b.Property<DateTime>("DeletedOn");
+
+                    b.Property<DateTime>("ModifiedOn");
+
+                    b.Property<int?>("RoleId");
+
+                    b.Property<int?>("UserId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("AppId");
+
+                    b.HasIndex("RoleId");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("UserAppRoles");
                 });
 
             modelBuilder.Entity("LegnicaIT.DataAccess.Models.UserApps", b =>
@@ -93,6 +142,21 @@ namespace DataAccess.Migrations
                     b.HasIndex("UserId");
 
                     b.ToTable("UserApps");
+                });
+
+            modelBuilder.Entity("LegnicaIT.DataAccess.Models.UserAppRole", b =>
+                {
+                    b.HasOne("LegnicaIT.DataAccess.Models.App", "App")
+                        .WithMany()
+                        .HasForeignKey("AppId");
+
+                    b.HasOne("LegnicaIT.DataAccess.Models.Role", "Role")
+                        .WithMany()
+                        .HasForeignKey("RoleId");
+
+                    b.HasOne("LegnicaIT.DataAccess.Models.User", "User")
+                        .WithMany()
+                        .HasForeignKey("UserId");
                 });
 
             modelBuilder.Entity("LegnicaIT.DataAccess.Models.UserApps", b =>
--- src/DataAccess/Migrations/20170104103904_init.cs
+++ src/DataAccess/Migrations/20170104103904_init.cs
@@ -26,6 +26,22 @@ namespace DataAccess.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(150)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Users",
                 columns: table => new
                 {
@@ -39,11 +55,47 @@ namespace DataAccess.Migrations
                     ModifiedOn = table.Column<DateTime>(nullable: false),
                     Name = table.Column<string>(type: "NVARCHAR(100)", nullable: true),
                     PasswordHash = table.Column<string>(type: "NVARCHAR(256)", nullable: false),
-                    PasswordSalt = table.Column<byte[]>(nullable: false)
+                    PasswordSalt = table.Column<string>(type: "NVARCHAR(128)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAppRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AppId = table.Column<int>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    DeletedOn = table.Column<DateTime>(nullable: false),
+                    ModifiedOn = table.Column<DateTime>(nullable: false),
+                    RoleId = table.Column<int>(nullable: true),
+                    UserId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAppRoles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAppRoles_Apps_AppId",
+                        column: x => x.AppId,
+                        principalTable: "Apps",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserAppRoles_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserAppRoles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
                 });
 
             migrationBuilder.CreateTable(
@@ -76,6 +128,21 @@ namespace DataAccess.Migrations
                 });
 
             migrationBuilder.CreateIndex(
+                name: "IX_UserAppRoles_AppId",
+                table: "UserAppRoles",
+                column: "AppId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAppRoles_RoleId",
+                table: "UserAppRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAppRoles_UserId",
+                table: "UserAppRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
                 name: "IX_UserApps_AppId",
                 table: "UserApps",
                 column: "AppId");
@@ -89,7 +156,13 @@ namespace DataAccess.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
+                name: "UserAppRoles");
+
+            migrationBuilder.DropTable(
                 name: "UserApps");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
 
             migrationBuilder.DropTable(
                 name: "Apps");
