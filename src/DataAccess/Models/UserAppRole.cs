548a0376c6fbc52f55a60a28d73acdc63fb8e0ea
Saibamen <saibamenppl@gmail.com>
2017-01-04 10:55:05 +0100

    AS-18: UserAppRoles and Roles tables and add auto migration in JwtDbContext

---
+++ src/DataAccess/Models/UserAppRole.cs
@@ -0,0 +1,9 @@
+﻿namespace LegnicaIT.DataAccess.Models
+{
+    public class UserAppRole : BaseEntity
+    {
+        public User User { set; get; }
+        public App App { set; get; }
+        public Role Role { set; get; }
+    }
+}
