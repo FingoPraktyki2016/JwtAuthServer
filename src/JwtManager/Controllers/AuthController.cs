d7075651822a45375acac0edc41e0ddce9888004
Szymon Matwijów <szymon.matwijow@gmail.com>
2017-01-04 14:46:07 +0100

    Add authorize filter attribute and db seeder class

--- src/JwtManager/Controllers/AuthController.cs
+++ src/JwtManager/Controllers/AuthController.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LegnicaIT.JwtManager.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LegnicaIT.JwtManager.Controllers
 {
-    public class AuthController
+    
+    public class AuthController : Controller
     {
+        [HttpGet("/auth")]
+        [AuthorizeFilter(UserRole.One, UserRole.Two)]
+        public string Index()
+        {
+            return "user have Permission";
+        }
 
-
+        [HttpGet("/auth/login")]
+        public string Login()
+        {
+            return "LoginPage";
+        }
     }
 }
