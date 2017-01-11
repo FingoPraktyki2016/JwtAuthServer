0043c5795781930b24fbf30cedc9afdd73c69289
SilentGreg <zundigrzech@gmail.com>
2017-01-11 15:27:27 +0100

    AS-11 Fix to migrator

--- src/JwtAuthServer/Startup.cs
+++ src/JwtAuthServer/Startup.cs
@@ -37,7 +37,7 @@ namespace LegnicaIT.JwtAuthServer
             var logLevel = (LogLevel)Enum.Parse(typeof(LogLevel), debugValue);
 
             //I'm gonna leave it as string array becase we might want to add some log modules later
-            string[] logOnlyThese = { "WebHost" }; // or reverse string[] dontlong = {"ObjectResultExecutor", "JsonResultExecutor"};
+            string[] logOnlyThese = { "WebHost" }; // or reverse string[] dontLog = {"ObjectResultExecutor", "JsonResultExecutor"};
 
             loggerFactory.AddDebug((category, _logLevel) => (logOnlyThese.Any(category.Contains) && _logLevel >= logLevel));
 
