2ee92813f7ee6164ac2a072f774f52a32905c253
Saibamen <saibamenppl@gmail.com>
2017-01-11 16:11:55 +0100

    AS-22: Added test for GetUserId by Rafał

---
+++ src/BusinessLogic.Tests/Actions/User/GetUserIdTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using LegnicaIT.BusinessLogic.Actions.User.Implementation;
+using LegnicaIT.DataAccess.Repositories.Interfaces;
+using Moq;
+using Xunit;
+
+namespace LegnicaIT.BusinessLogic.Tests.Actions.User
+{
+    public class GetUserIdTests
+    {
+        [Fact]
+        public void Invoke_Returns_CorrectId()
+        {
+            // prepare
+            var dataUser = new DataAccess.Models.User() { Id = 1234 };
+            var findByResult = new List<DataAccess.Models.User>() { dataUser };
+            var mockedUserRepository = new Mock<IUserRepository>();
+            mockedUserRepository.Setup(r => r.FindBy(It.IsAny<Expression<Func<DataAccess.Models.User, bool>>>()))
+                .Returns(findByResult.AsQueryable);
+            var action = new GetUserId(mockedUserRepository.Object);
+
+            // action
+            var userId = action.Invoke("email@dot.com");
+
+            // check
+            Assert.Equal(1234, userId);
+        }
+    }
+}
