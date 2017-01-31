3c9acd0889c2953f33176ee56f0a62420a1cc639
Radziejewskig <radziejewski.g@gmail.com>
2017-01-31 09:34:04 +0100

    AS-27: Added ApplicationController

---
+++ src/JwtManager/Controllers/ApplicationController.cs
@@ -0,0 +1,72 @@
+﻿using LegnicaIT.BusinessLogic.Helpers;
+using LegnicaIT.JwtManager.Configuration;
+using Microsoft.Extensions.Options;
+using LegnicaIT.JwtManager.Authorization;
+using LegnicaIT.BusinessLogic.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LegnicaIT.JwtManager.Controllers
+{
+    public class ApplicationController : BaseController
+    {
+        public ApplicationController(
+            IOptions<ManagerSettings> managerSettings,
+            IOptions<LoggerConfig> loggerSettings)
+            : base(managerSettings, loggerSettings)
+        {
+        }
+
+        [AuthorizeFilter(UserRole.Manager)]
+        [HttpPost("index")]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string s)
+        {
+
+
+            return View();
+        }
+
+
+        [AuthorizeFilter(UserRole.Manager)]
+        [HttpGet("index")]
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+
+        [AuthorizeFilter(UserRole.Manager)]
+        [HttpPost("adduser")]
+        public IActionResult AddUser(string s)
+        {
+
+            return View();
+        }
+
+
+        [AuthorizeFilter(UserRole.Manager)]
+        [HttpGet("adduser")]
+        public IActionResult AddUser()
+        {
+
+
+            return View();
+        }
+
+        [AuthorizeFilter(UserRole.Manager)]
+        [HttpPost("adduser")]
+        public IActionResult EditUser(string s)
+        {
+
+
+            return View();
+        }
+
+
+
+
+
+    }
+}
