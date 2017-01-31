5b4f61994e51bad27436460d139a8cd6533e6717
Radziejewskig <radziejewski.g@gmail.com>
2017-01-31 15:17:55 +0100

    AS-27: Fixed getting user apps

---
+++ src/BusinessLogic/Actions/App/Interfaces/IGetUserApps.cs
@@ -0,0 +1,13 @@
+﻿
+using LegnicaIT.BusinessLogic.Actions.Base;
+using LegnicaIT.BusinessLogic.Models;
+using System.Collections.Generic;
+
+namespace LegnicaIT.BusinessLogic.Actions.App.Interfaces
+{
+    public interface IGetUserApps : IAction
+
+    {
+        List<AppModel>Invoke (int userId);
+    }
+}
--- src/JwtManager/Controllers/ApplicationController.cs
+++ src/JwtManager/Controllers/ApplicationController.cs
@@ -1,49 +1,43 @@
-﻿using LegnicaIT.BusinessLogic.Helpers;
-using LegnicaIT.JwtManager.Configuration;
+﻿using LegnicaIT.JwtManager.Configuration;
 using Microsoft.Extensions.Options;
 using LegnicaIT.JwtManager.Authorization;
 using LegnicaIT.BusinessLogic.Enums;
 using Microsoft.AspNetCore.Mvc;
 using LegnicaIT.JwtManager.Models;
+using LegnicaIT.BusinessLogic.Helpers;
+using LegnicaIT.BusinessLogic.Actions.App.Interfaces;
 
 namespace LegnicaIT.JwtManager.Controllers
 {
     [AuthorizeFilter(UserRole.Manager)]
     public class ApplicationController : BaseController
     {
+        private readonly IGetUserApps getUserApps;
+
         public ApplicationController(
+            IGetUserApps getUserApps,          
             IOptions<ManagerSettings> managerSettings,
             IOptions<LoggerConfig> loggerSettings)
             : base(managerSettings, loggerSettings)
         {
-        }
+             this.getUserApps = getUserApps;
+    }
 
         //[HttpGet("index")]
         public IActionResult Index()
         {
-
-
-            //TODO List of all user apps
-            //  var models = new List<AppModel>( );  // TODO how to get all user apps from logged user
-
-
-            var model = new AppModel()
-            {
-                Id = 1,
-                Name = "App1"
-            };
+            var userApps = getUserApps.Invoke(LoggedUser.Id);
 
             //TODO A View with list of applications
-            return View(new FormModel<AppModel>(false,model));
-
-          //  return View();
+            //  return View(new FormModel<AppModel>(false,userApps));
+            return Json(userApps);
+            
         }
 
         [HttpPost("adduser")]
         [ValidateAntiForgeryToken]
         public IActionResult AddUser(UserAppModel appuser)
         {
-           
             //TODO Add new app user. Go to Index or refresh view?
             return View();
         }
