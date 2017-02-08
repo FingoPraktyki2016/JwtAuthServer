db8cdd6bc54a2ddea7ce637265bc667763670c49
rafal <rafal.gradziel@fingo.pl>
2017-02-08 14:52:35 +0100

    AS-41: Sample AngularJS action

---
+++ src/JwtManager/Controllers/AngularController.cs
@@ -0,0 +1,32 @@
+﻿using LegnicaIT.BusinessLogic.Actions.App.Interfaces;
+using LegnicaIT.BusinessLogic.Helpers;
+using LegnicaIT.JwtManager.Configuration;
+using LegnicaIT.JwtManager.Models;
+using LegnicaIT.JwtManager.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LegnicaIT.JwtManager.Controllers
+{
+    public class AngularController : BaseController
+    {
+        public AngularController(
+            IOptions<ManagerSettings> managerSettings,
+            IOptions<LoggerConfig> loggerSettings,
+            IGetUserApps getUserApps,
+            ISessionService<LoggedUserModel> loggedUserSessionService
+        )
+            : base(managerSettings, loggerSettings, getUserApps, loggedUserSessionService)
+        {
+            
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
