01270f298a843a0a62070cc47d0ff562950348b5
Radziejewskig <radziejewski.g@gmail.com>
2017-02-13 10:49:21 +0100

    AS-52: Added tests

--- src/BusinessLogic.Tests/Actions/User/DeleteUserTests.cs
+++ src/BusinessLogic.Tests/Actions/User/DeleteUserTests.cs
@@ -2,6 +2,8 @@
 using LegnicaIT.BusinessLogic.Models;
 using LegnicaIT.DataAccess.Repositories.Interfaces;
 using Moq;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace LegnicaIT.BusinessLogic.Tests.Actions.User
@@ -21,10 +23,27 @@ namespace LegnicaIT.BusinessLogic.Tests.Actions.User
                 Id = 1,
             };
 
+            var userapp1 = new DataAccess.Models.UserApps
+            {
+                App = new DataAccess.Models.App
+                {
+                    Id = 1,
+                    Name = "app1"
+                },
+                User = new DataAccess.Models.User { Id = 1 }
+            };
+
+            var applist = new List<DataAccess.Models.UserApps> { userapp1 };
+
             var mockedUserRepository = new Mock<IUserRepository>();
+            var mockedUserAppRepository = new Mock<IUserAppRepository>();
+
             mockedUserRepository.Setup(r => r.GetById(1)).Returns(userFromDB);
 
-            var action = new DeleteUser(mockedUserRepository.Object);
+            mockedUserAppRepository.Setup(r => r.GetAll()).Returns(applist.AsQueryable);
+
+
+            var action = new DeleteUser(mockedUserRepository.Object, mockedUserAppRepository.Object);
 
             // action
             var actionResult = action.Invoke(userToDelete.Id);
@@ -33,20 +52,42 @@ namespace LegnicaIT.BusinessLogic.Tests.Actions.User
             Assert.True(actionResult);
             mockedUserRepository.Verify(r => r.Delete(It.IsAny<DataAccess.Models.User>()), Times.Once());
             mockedUserRepository.Verify(r => r.Save(), Times.Once());
+            mockedUserAppRepository.Verify(r => r.Delete(It.IsAny<DataAccess.Models.UserApps>()), Times.Once());
+            mockedUserAppRepository.Verify(r => r.Save(), Times.Once());
         }
 
         [Fact]
         public void Invoke_ValidData_DeleteNorSaveAreCalled()
         {
             // prepare
-            var userToDelete = new UserModel()
-            {
-                Id = 1
-            };
-            var mockedUserRepository = new Mock<IUserRepository>();
-            mockedUserRepository.Setup(r => r.GetById(1)).Returns((DataAccess.Models.User)null);
+            var userToDelete = new UserModel();
 
-            var action = new DeleteUser(mockedUserRepository.Object);
+            var userFromDB = new DataAccess.Models.User()
+            {
+                Id = 2,
+            };
+
+            var userapp1 = new DataAccess.Models.UserApps
+            {
+                App = new DataAccess.Models.App
+                {
+                    Id = 1,
+                    Name = "app1"
+                },
+                User = new DataAccess.Models.User { Id = 3 }
+            };
+
+            var applist = new List<DataAccess.Models.UserApps> { userapp1 };
+
+            var mockedUserRepository = new Mock<IUserRepository>();
+            var mockedUserAppRepository = new Mock<IUserAppRepository>();
+
+
+            mockedUserRepository.Setup(r => r.GetById(1)).Returns(userFromDB);
+
+            mockedUserAppRepository.Setup(r => r.GetAll()).Returns(applist.AsQueryable);
+
+            var action = new DeleteUser(mockedUserRepository.Object, mockedUserAppRepository.Object);
 
             // action
             var actionResult = action.Invoke(userToDelete.Id);
@@ -55,6 +96,9 @@ namespace LegnicaIT.BusinessLogic.Tests.Actions.User
             Assert.False(actionResult);
             mockedUserRepository.Verify(r => r.Delete(It.IsAny<DataAccess.Models.User>()), Times.Never);
             mockedUserRepository.Verify(r => r.Save(), Times.Never);
+            mockedUserAppRepository.Verify(r => r.Delete(It.IsAny<DataAccess.Models.UserApps>()), Times.Never);
+            mockedUserAppRepository.Verify(r => r.Save(), Times.Never);
         }
+        
     }
 }
