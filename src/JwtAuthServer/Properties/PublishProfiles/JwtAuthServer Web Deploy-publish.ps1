7055dc0a44335def9ecce7277c7b7747179a2912
Saibamen <saibamenppl@gmail.com>
2017-02-13 16:31:03 +0100

    AS-43: Added bundleconfig.json to build, change Bundle Minifier startup in project.json, introduce other

---
+++ src/JwtAuthServer/Properties/PublishProfiles/JwtAuthServer Web Deploy-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'bd26e70e-3b3b-4cde-b0e3-b756dc11a96e'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
--- src/JwtManager/Startup.cs
+++ src/JwtManager/Startup.cs
@@ -23,8 +23,8 @@ namespace LegnicaIT.JwtManager
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: false, reloadOnChange: true);
 
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
